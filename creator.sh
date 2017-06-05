b31a9b60fbb7b8e235ed276f95d5ba93e68a856c
Diego Ernst <dernst@xmartlabs.com>
2017-06-05 12:06:29 -0300

    Changed git clone to master branch

--- creator.sh
+++ creator.sh
@@ -2,7 +2,7 @@
 uuid=$(uuidgen)
 tmpfolder="/tmp/$uuid"
 mkdir $tmpfolder
-git clone -b project-template git@github.com:xmartlabs/Swift-Base-Project.git $tmpfolder
+git clone git@github.com:xmartlabs/Swift-Base-Project.git $tmpfolder
 rm "$tmpfolder/ProjectTemplate/Project-iOS/XLProjectName/Podfile.lock"
 script="$tmpfolder/ProjectTemplate/shell.swift"
 swift -target x86_64-apple-macosx10.11 $script
